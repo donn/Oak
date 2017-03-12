121ddfd275925980c343f8bb0b60d86049e263fc
Skyus <ekaskyus@gmail.com>
2017-03-12 11:54:08 +0200

     Fixed macOS compatibility

--- Package.swift
+++ Package.swift
@@ -11,5 +11,5 @@ let package = Package (
         .Package(url: "https://github.com/Skyus/Colors.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/oarrabi/Guaka.git", majorVersion: 0)
     ],
-    exclude: ["Resources", "Documentation", "Samples"]
+    exclude: ["Build", "DerivedData", "Resources", "Documentation", "Samples"]
 )
